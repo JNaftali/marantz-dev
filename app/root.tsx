b51af595ff6cef585ffdcc683f451e429e637e85
Josh Marantz <josh@marantz.dev>
2022-05-28 22:28:49 -0400

    tweak

--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'Remix Notes',
+  title: "Josh Marantz's site",
   viewport: 'width=device-width,initial-scale=1',
 });
 
